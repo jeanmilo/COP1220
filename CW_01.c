6b96152e5531348d736f4a9f96973da15ffc05c9
Claudia <claudia.parlee@gmail.com>
2020-09-08 10:37:44 -0400

    Create CW_01.c

---
+++ CW_01.c
@@ -0,0 +1,19 @@
+/*
+* Program: P_CW_001
+* Purpose: This program prints a welcome message
+* Objectives: 1) Comments; 2) Preprocessor directive (#include); 3) main() function; 4) printf() function
+* Class: COP1220: Introduction to Programming in C; Summer 2020; MW 0800AM - 1145AM 
+* Instructor: Dr. Fazelpour - Copyright (c) 2020
+* Programmers: Claudia Yamamoto
+* Date: 14 July 2020 
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  /* statements */
+  printf("Welcome to C Programming Language");
+
+  return(0);
+}
